2cfc117332532fc13d98de733eed1f30acda538d
Paul Wagstaff <paul@pwfotos.ca>
2025-11-22 19:51:43 -0700

    Initial Commit

---
+++ WomenInComputing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WomenInComputing
+//
+//  Created by Paul Wagstaff on 2025-11-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WomenInComputing/WomenInComputingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WomenInComputingApp.swift
+//  WomenInComputing
+//
+//  Created by Paul Wagstaff on 2025-11-22.
+//
+
+import SwiftUI
+
+@main
+struct WomenInComputingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
